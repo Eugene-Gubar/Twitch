87d49747ea858467adb6a52afcf0b84ece76b9c6
Eugene <eugene.gubar@gmail.com>
2017-02-10 23:51:14 +0200

    Add enter, css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	var channels = ["freecodecamp","test_channel","ESL_SC2"];
+	var channels = ["freecodecamp","test_channel","ESL_SC2", "syndicate", "riotgames", "esl_csgo", "nightblue3", "summit1g", "imaqtpie", "lirik"];
 
 	function getTwitchJSON(name) {
 		var urlStreams = 'https://wind-bow.gomix.me/twitch-api/streams/' + name + '?callback=?';
@@ -35,48 +35,62 @@ $(document).ready(function() {
 
 	}
 
+	var initBtn = function() {
 
+		$('#all').on('click', function() {
+			$('.list-group>a').removeClass('hidden');
+		});
+
+		$('#online').on('click', function() {
+			$('.online').each(function() {
+				$(this).removeClass('hidden');
+			});
+
+			$('.offline').each(function() {
+				$(this).addClass('hidden');
+			});
+		});
+
+		$('#offline').on('click', function() {
+			$('.online').each(function() {
+				$(this).addClass('hidden');
+			});
+
+			$('.offline').each(function() {
+				$(this).removeClass('hidden');
+			});
+		});
+
+		$('#btn-clear').on('click', function() {
+			$('.list-group').empty();
+		});
+
+
+		$("#btn-send").on('click', function(e) {
+
+			var value = $("#inp-search").val().replace(/\s/g, '');
+
+			if (value !== '') {
+				var channels = value.split(',');
+				channels.forEach(function(element) {
+					getTwitchJSON(element);
+				});
+			}
+
+		});
+
+		$('input').keypress(function(e) {
+			if(e.which == 13) {
+				$('#btn-send').click();
+			}
+		});
+
+	};
 
 	channels.forEach(function(element) {
 		getTwitchJSON(element);
 	});
 
-
-	$('#all').on('click', function() {
-		$('.list-group>a').removeClass('hidden');
-	});
-	$('#online').on('click', function() {
-		$('.online').each(function() {
-			$(this).removeClass('hidden');
-		});
-		$('.offline').each(function() {
-			$(this).addClass('hidden');
-		});
-	});
-	$('#offline').on('click', function() {
-		$('.online').each(function() {
-			$(this).addClass('hidden');
-		});
-		$('.offline').each(function() {
-			$(this).removeClass('hidden');
-		});
-	});
-
-	
-	$("#btn-send").on('click', function() {
-
-		var value = $("#inp-search").val().replace(/\s/g, '');
-
-		if (value !== '') {
-			var channels = value.split(',');
-			console.log(channels);
-			channels.forEach(function(element) {
-				console.log(element);
-				getTwitchJSON(element);
-			});
-		}
-
-	});
-
+	initBtn();
 
 });
