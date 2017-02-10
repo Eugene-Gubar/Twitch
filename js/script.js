37f24b62d2690731beaed89b11338ce194853715
Eugene <eugene.gubar@gmail.com>
2017-02-10 13:33:57 +0200

    Start function getTwitchJSON()

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-	
+
+	var channels = ["freecodecamp","test_channel","ESL_SC2"];
+
+	function getTwitchJSON(name) {
+		var urlStreams = 'https://wind-bow.gomix.me/twitch-api/streams/' + name + '?callback=?';
+		var urlChannels = 'https://wind-bow.gomix.me/twitch-api/channels/' + name + '?callback=?';
+
+			$.getJSON(urlStreams, function(streams) {
+				var game, status;
+
+				if (streams.stream === null) {
+					game = "Offline";
+					status = "offline";
+				} else if (streams.stream === undefined) {
+					game = "Account Closed";
+					status = "offline";
+				} else {
+					game = streams.stream.game;
+					status = "online";
+				}
+
+				$.getJSON(urlChannels, function(channels) {
+
+					console.log(game, status);
+					console.log(channels);
+
+				});
+				
+			});
+
+	}
+
+
+
+	getTwitchJSON('ESL_SC2');
+
+
 });
